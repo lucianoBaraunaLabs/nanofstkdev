0938cfb4056feb5c2af60f2445becd70a43b58e6
Luciano Baraúna <baraunaluciano@gmail.com>
2019-02-02 20:42:09 -0200

    comecando aula 09

---
+++ 01-fundamentos-progweb/aula09/entertainment_center.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import media
+
+# Instancia de variábel toy_story
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+# print(toy_story.storyline)
+avatar = media.Movie("Avatar",
+        "A marine on an alien planet",
+        "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+        "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+avenger_infintywar = media.Movie("Avengers: Infinity War",
+                    "Having acquired the Power Stone, one of the six Infinity Stones",
+                    "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg",
+                    "https://www.youtube.com/watch?v=t_ULBP6V9bg")
+
+school_of_rock = media.Movie("School of Rock",
+                "Storyline",
+                "https://upload.wikimedia.org/wikipedia/en/thumb/1/11/School_of_Rock_Poster.jpg/220px-School_of_Rock_Poster.jpg",
+                "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Avengers: Infinity War",
+              "Storyline",
+              "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+              "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Avengers: Infinity War",
+                    "Storyline",
+                    "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                    "https://www.youtube.com/watch?v=FAfR8omt-CY")
+
---
+++ 01-fundamentos-progweb/aula09/media.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import webbrowser
+
+class Movie(object):
+    # Init é o construtor da classe.
+    # O argumento self significa a própria classe o famoso this no JS.
+    def __init__(self, movie_title, movie_storyline, poster_image, 
+                trailer_youtube):
+        # Passando os argumentos correspondentes 
+        # para criar as instancias das variáveis.
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+    
+    # Criando método da instância para mostrar o trailer
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
